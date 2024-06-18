14ed46a0191f7b8a82f5cd9503e55de5788389c4
psora <psora93@gmail.com>
2024-06-18 19:39:50 +0900

    feat:Embedded Mode

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Slf4j
 //@Import(MemoryConfig.class)
@@ -27,5 +30,17 @@ public class ItemServiceApplication {
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
 	}
+
+//	@Bean
+//	@Profile("test")
+//	public DataSource dataSource(){
+//		log.info("메모리 데이터베이스 초기화");
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.h2.Driver");  // h2 데이터베이스 지정
+//		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1"); // jdbc:h2:mem:db : 임베디드 모드(메모리 모드)
+//		dataSource.setUsername("sa");
+//		dataSource.setPassword("");
+//		return dataSource;
+//	}
 
 }
